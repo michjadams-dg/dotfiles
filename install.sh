c42959873c0ba02ae8b7b172c85b1fdab1fec9a6
michjadams-dg <michaela.adams@deepgram.com>
2026-03-25 15:05:33 -0400

    add nix to path in install.sh file

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 curl -fsSL https://nixos.org/nix/install | sh -s -- --no-daemon
+NIX_LINE='[ -e ~/.nix-profile/etc/profile.d/nix.sh ] && . ~/.nix-profile/etc/profile.d/nix.sh'
+
+# Create .bashrc if it doesn't exist, then append nix sourcing if not already there
+touch ~/.bashrc
+
+if ! grep -qF 'nix-profile/etc/profile.d/nix.sh' ~/.bashrc; then
+  echo "$NIX_LINE" >> ~/.bashrc
+fi
